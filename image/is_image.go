e0909784a4eb1a47f9e8e7494f3ed14a3688cf9b
kcjia <otkinlife@163.com>
2024-07-22 18:42:02 +0800

    is_image

--- image/is_image.go
+++ image/is_image.go
@@ -5,7 +5,7 @@ import "strings"
 func IsImage(suffix string) bool {
 	suffix = strings.ToLower(suffix)
 	switch suffix {
-	case ".jpg", ".jpeg", ".png", ".gif", ".bmp", ".webp":
+	case "jpg", "jpeg", "png", "gif", "bmp", "webp":
 		return true
 	default:
 		return false
