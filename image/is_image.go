a2cf5da421dfc85941a227b7ef60b3f398dff747
kcjia <otkinlife@163.com>
2024-07-22 17:59:04 +0800

    is_image

---
+++ image/is_image.go
@@ -0,0 +1,13 @@
+package image
+
+import "strings"
+
+func IsImage(suffix string) bool {
+	suffix = strings.ToLower(suffix)
+	switch suffix {
+	case ".jpg", ".jpeg", ".png", ".gif", ".bmp", ".webp":
+		return true
+	default:
+		return false
+	}
+}
