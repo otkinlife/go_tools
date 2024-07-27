e4b0c80b55d3f23a9dd7e7ccf455a36f2b43a075
kcjia <otkinlife@163.com>
2024-07-27 12:29:22 +0800

    list diff

--- list/list_test.go
+++ list/list_test.go
@@ -23,3 +23,12 @@ func TestInList(t *testing.T) {
 	}, []TItem{{A: 1, B: "a"}, {A: 2, B: "b"}})
 	t.Log("ret:", ret)
 }
+
+func TestFindDifferenceNotInSlice2(t *testing.T) {
+	// 字符串
+	ret := FindDifferenceNotInSlice2([]string{"a", "b", "c"}, []string{"a", "c"})
+	t.Log("ret:", ret)
+	// 数字
+	ret2 := FindDifferenceNotInSlice2([]int{1, 2, 3}, []int{1, 3})
+	t.Log("ret:", ret2)
+}
