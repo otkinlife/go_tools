9af755cfddecd42b275780cc078c3145b8abd7dc
kcjia <otkinlife@163.com>
2024-11-28 12:14:32 +0800

    http_tools

--- http_tools/client.go
+++ http_tools/client.go
@@ -3,7 +3,6 @@ package http_tools
 import (
 	"log"
 	"net/http"
-	"net/url"
 )
 
 type ReqClient struct {
@@ -21,60 +20,12 @@ func NewReqClient(method, url string) *ReqClient {
 	}
 }
 
-// Get 发送 GET 请求
-// urlStr: 请求地址
-// return: 错误
-func (r *ReqClient) Get(urlStr string) error {
-	return r.Send("GET", urlStr)
-}
-
-// Post 发送 POST 请求
-// urlStr: 请求地址
-// return: 错误
-func (r *ReqClient) Post(urlStr string) error {
-	return r.Send("POST", urlStr)
-}
-
-// GetRetry 发送 GET 请求，重试
-// urlStr: 请求地址
-// tryCount: 重试次数
-// return: 错误
-func (r *ReqClient) GetRetry(urlStr string, tryCount int) error {
-	var err error
-	for i := 0; i < tryCount; i++ {
-		err = r.Get(urlStr)
-		if err == nil && r.GetHttpCode() == 200 {
-			break
-		}
-	}
-	return err
-}
-
-// PostRetry 发送 POST 请求，重试
-// urlStr: 请求地址
-// tryCount: 重试次数
-func (r *ReqClient) PostRetry(urlStr string, tryCount int) error {
-	var err error
-	for i := 0; i < tryCount; i++ {
-		err = r.Post(urlStr)
-		if err == nil && r.GetHttpCode() == 200 {
-			break
-		}
-	}
-	return err
-}
-
 // Send 发送请求
 // method: 请求方法
 // urlStr: 请求地址
 // return: 错误
-func (r *ReqClient) Send(urlStr string) error {
+func (r *ReqClient) Send() error {
 	var err error
-	r.req.Method = method
-	r.req.URL, err = url.Parse(urlStr)
-	if err != nil {
-		return err
-	}
 	if r.isPrintCurl {
 		log.Println(ConvertToCurlString(*r.req))
 	}
--- http_tools/http_test.go
+++ http_tools/http_test.go
@@ -5,7 +5,7 @@ import "testing"
 func TestSendGet(t *testing.T) {
 	t.Log("TestSendGet")
 	client := NewReqClient("GET", "http://www.baidu.com")
-	err := client.Send("http://www.baidu.com")
+	err := client.Send()
 	if err != nil {
 		t.Error(err)
 	}
