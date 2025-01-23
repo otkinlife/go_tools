c90a9c9d41d30b3c4d2268d27c53228c400eb9c5
kcjia <otkinlife@163.com>
2025-01-23 16:27:14 +0800

    img touch

--- img/img_base64.go
+++ img/img_base64.go
@@ -30,14 +30,30 @@ func LocalImageToDataURL(imagePath string) (string, error) {
 }
 
 func DecodeBase642Img(base64Str, imgPath string) error {
-	// Decode the base64 string
+	// 解码 Base64 字符串
 	imageData, err := base64.StdEncoding.DecodeString(base64Str)
 	if err != nil {
 		return err
 	}
 
-	// Write the image data to a file
-	err = os.WriteFile(imgPath, imageData, 0644)
+	// 获取文件的目录路径
+	dir := filepath.Dir(imgPath)
+
+	// 创建目录（如果不存在）
+	err = os.MkdirAll(dir, 0755)
+	if err != nil {
+		return err
+	}
+
+	// 打开或创建文件
+	file, err := os.OpenFile(imgPath, os.O_WRONLY|os.O_CREATE|os.O_TRUNC, 0644)
+	if err != nil {
+		return err
+	}
+	defer file.Close()
+
+	// 将图像数据写入文件
+	_, err = file.Write(imageData)
 	if err != nil {
 		return err
 	}
