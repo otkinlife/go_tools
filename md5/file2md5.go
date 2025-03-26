ad5872a7afb6dc0595a53327d79fe48043578c9b
kcjia <otkinlife@163.com>
2025-03-26 14:56:24 +0800

    Add function to generate MD5 from file URL

--- md5/file2md5.go
+++ md5/file2md5.go
@@ -3,6 +3,7 @@ package md5
 import (
 	"crypto/md5"
 	"encoding/hex"
+	"github.com/otkinlife/go_tools/downloader"
 	"io"
 	"os"
 )
@@ -12,6 +13,27 @@ import (
 // return: MD5字符串
 func FileGenerate(filePath string) (string, error) {
 	file, err := os.Open(filePath)
+	if err != nil {
+		return "", err
+	}
+	defer file.Close()
+	hash := md5.New()
+	if _, err := io.Copy(hash, file); err != nil {
+		return "", err
+	}
+	hashBytes := hash.Sum(nil)
+	return hex.EncodeToString(hashBytes), nil
+}
+
+// FileUrlGenerate 基于文件URL生成MD5
+// url: 文件URL
+func FileUrlGenerate(url, saveFileName string) (string, error) {
+	err := downloader.DownloadFile(url, saveFileName, 8)
+	if err != nil {
+		return "", err
+	}
+	defer os.Remove(saveFileName)
+	file, err := os.Open(saveFileName)
 	if err != nil {
 		return "", err
 	}
--- md5/md5_test.go
+++ md5/md5_test.go
@@ -14,3 +14,11 @@ func TestStringGenerate(t *testing.T) {
 	m := StringGenerate("hello world")
 	t.Log("md5:", m)
 }
+
+func TestFileUrlGenerate(t *testing.T) {
+	m, err := FileUrlGenerate("https://bi-material-center.oss-cn-beijing.aliyuncs.com/materials_center/videos/3c491c3372ac383965e9a8202eb4e3a6/3c491c3372ac383965e9a8202eb4e3a6.mp4", "3c491c3372ac383965e9a8202eb4e3a6.mp4")
+	if err != nil {
+		t.Fatal(err)
+	}
+	t.Log("md5:", m)
+}
